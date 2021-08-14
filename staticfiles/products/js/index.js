c964a0cef1cc99621f200aae56184bdd094fbdc9
Charls Catipay <charlscatipaycoc@gmail.com>
2021-08-14 13:10:36 +0800

    static files

---
+++ staticfiles/products/js/index.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('.navigations').find('ul li').removeAttr('active');
+    $('.navigations').find('ul li.li-index').addClass('active');  
+})
---
+++ staticfiles/products/js/products.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('.navigations').find('ul li').removeAttr('active');
+    $('.navigations').find('ul li.li-products').addClass('active');  
+})
